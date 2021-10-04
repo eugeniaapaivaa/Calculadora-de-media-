85229b680f6fd4b33fb5523d177e1aa8640d2773
Maria Eugênia Paiva <90074242+eugeniaapaivaa@users.noreply.github.com>
2021-10-04 14:56:15 -0300

    Adicionei os arquivos da calculadora de média

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome = "Eugenia Paiva"
+var notaDoPrimeiroBimestre = 7
+var notaDoSegundoBimestre = 5
+var notaDoTerceiroBimestre = 8
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre) / 3
+
+var notaFinal = 6.0
+
+var resultado = "Parabéns, Você foi Aprovada!"
+
+console.log("Bem Vinda " + nome)
+console.log(notaFinal)
+console.log (resultado)
